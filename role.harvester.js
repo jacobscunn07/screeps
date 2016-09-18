46b15d7b2526151d9c70bb500175f6a6b28f58ec
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-17 23:24:22 -0500

    no message

--- role.harvester.js
+++ role.harvester.js
@@ -4,7 +4,7 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        var targets = var targets = creep.room.find(FIND_STRUCTURES, {
+        var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
@@ -29,14 +29,6 @@ var roleHarvester = {
                 }
             }
             else {
-                var targets = creep.room.find(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_EXTENSION ||
-                                    structure.structureType == STRUCTURE_SPAWN ||
-                                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                        }
-                });
-
                 if(targets.length > 0) {
                     if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0]);
