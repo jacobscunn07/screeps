cfc621d9ecf406e1ba8238f29056061b28e447ef
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-17 20:28:30 -0500

    use minby instead of orderby

--- global.helpers.js
+++ global.helpers.js
@@ -14,8 +14,9 @@ var helpers = {
 				var count = _.filter(Game.creeps, function(c) { return c.targetSourceId == source.id }).length;
 				sourceCounts.push({source: source, count: count});
 			});
-			sourceCounts = _.orderBy(sourceCounts, ['count']);
-			var source = sourceCounts[0].source;
+			//sourceCounts = _.orderBy(sourceCounts, ['count']);
+			var source = _.minBy(sourceCounts, function(s){return s.count})[0].source;
+			//var source = sourceCounts[0].source;
 			creep.memory.targetSourceId = source.id;
 			return source;
 		}
