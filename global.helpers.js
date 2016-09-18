105cd27b0ae196db24ae5b5ac675c3b8f0b9080c
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-17 21:26:31 -0500

    no message

--- global.helpers.js
+++ global.helpers.js
@@ -5,7 +5,6 @@ var helpers = {
 	{
 		if(creep.memory.targetSourceId) {
 			var source = Game.getObjectById(creep.memory.targetSourceId);
-			console.log(creep.name + " heading to source " + source.id);
 			return source;
 		}
 		else
@@ -17,9 +16,7 @@ var helpers = {
 				var sourceObject = {source: source, count: count};
 				sourceCounts.push(sourceObject);
 			});
-			console.log('Sources: ' + JSON.stringify(sourceCounts));
 			var s = _.min(sourceCounts, 'count');
-			console.log('Source: ' + JSON.stringify(s));
 			s = s.source;
 			creep.memory.targetSourceId = s.id;
 			return s;
