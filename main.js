d900e5217e80964be7943f500d0b6074e9f42260
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-18 10:02:24 -0500

    no message

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleRepairer = require('role.repairer');
 var helpers = require('global.helpers');
 
 module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer')
     
     if(harvesters.length < 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
     
     if(upgraders.length < 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
 
-    if(builders.length < 4) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
+    if(builders.length < 2) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
+    }
+
+    if(repairers.length < 2) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE], undefined, {role: 'repairer'});
+        console.log('Spawning new repairer: ' + newName);
     }
     
     for(var name in Game.creeps) {
---
+++ role.repairer.js
@@ -0,0 +1,39 @@
+var helpers = require('global.helpers');
+
+var roleRepairer = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.repairing && creep.carry.energy == 0) {
+            creep.memory.repairing = false;
+            creep.say('harvesting');
+	    }
+	    if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.repairing = true;
+	        creep.say('repairing');
+	    }
+
+	    if(creep.memory.repairing) {
+            var structure = creep.pos.findNearest(Game.STRUCTURES, {
+                    filter: function(structure) {
+                        return structure.hits < structure.hitsMax / 2;
+                    }
+                });
+
+
+            if(creep.moveTo(structure) == ERR_NOT_IN_RANGE) {
+                creep.say("repairing...")
+                creep.repair(structure);
+            }
+        }
+        else {
+            var source = helpers.findSource(creep);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+        }
+	}
+};
+
+module.exports = roleRepairer;
