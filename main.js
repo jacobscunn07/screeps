f115eb02e3002432feafd2ed2cfa0401f777145a
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-18 20:22:12 -0500

    no message

--- main.js
+++ main.js
@@ -15,28 +15,28 @@ module.exports.loop = function () {
         console.log('Spawning new harvester: ' + newName);
     }
     
-    if(upgraders.length < 2) {
+    if(upgraders.length < 4) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
 
-    if(Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES).length > 0 && builders.length < 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
-        console.log('Spawning new builder: ' + newName);
-    }
-    else if (Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES).length == 0) {
-        _.forEach(builders, function(builder) { builder.suicide(); });
-    }
+    // if(Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES).length > 0 && builders.length < 2) {
+    //     var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
+    //     console.log('Spawning new builder: ' + newName);
+    // }
+    // else if (Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES).length == 0) {
+    //     _.forEach(builders, function(builder) { builder.suicide(); });
+    // }
 
 
-    var needsRepairCount = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {filter: function(s){return s.hits < (s.hitsMax*.45)}}).length;
-    if(needsRepairCount > 0 && repairers.length < 1) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'repairer'});
-        console.log('Spawning new repairer: ' + newName);
-    }
-    else if (needsRepairCount == 0) {
-        _.forEach(repairers, function(repairer){ repairer.suicide(); });
-    }
+    // var needsRepairCount = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {filter: function(s){return s.hits < (s.hitsMax*.45)}}).length;
+    // if(needsRepairCount > 0 && repairers.length < 1) {
+    //     var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'repairer'});
+    //     console.log('Spawning new repairer: ' + newName);
+    // }
+    // else if (needsRepairCount == 0) {
+    //     _.forEach(repairers, function(repairer){ repairer.suicide(); });
+    // }
     
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
