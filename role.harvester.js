a317e0fb5c3e4b61320f2acc6e9dc7d28465f609
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-19 20:38:35 -0500

    no message

--- role.harvester.js
+++ role.harvester.js
@@ -22,16 +22,26 @@ var roleHarvester = {
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            var energy = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+
+            if(energy)
+            {
+                if(creep.pickup(energy) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
+                }
+            }
+            else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    }
+                });
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0]);
+                    }
                 }
             }
         }
