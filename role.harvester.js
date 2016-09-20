53182436ab7f5b4ee15448c9c5c36d19e09f0f0a
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-19 20:57:10 -0500

    no message

--- role.harvester.js
+++ role.harvester.js
@@ -4,50 +4,50 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+        if(creep.carry.energy < creep.carryCapacity) {
             // var source = helpers.findSource(creep);
             // if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
             //     creep.moveTo(source);
             // }
-
-            var containers = Game.spawns.Spawn1.room.find(FIND_STRUCTURES, { 
-                filter: function(c) { 
-                    return c.structureType == STRUCTURE_CONTAINER && 
-                        c.store.energy > 0;
-                    }
-                });
-            var container = creep.pos.findClosestByRange(containers);
-            if(creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(container);    
-            }
-        }
-        else {
             var energy = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
-            JSON.stringify(energy);
+            console.log(JSON.stringify(energy));
             if(energy)
             {
-                console.log("if energy");
+                //console.log(creep.pickup(energy));
                 if(creep.pickup(energy) == ERR_NOT_IN_RANGE) {
                     console.log("if pickup == err not in range");
-                    creep.moveTo(targets[0]);
+                    creep.moveTo(energy);
                 }
             }
             else {
-                var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                    }
-                });
-                if(targets.length > 0) {
-                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0]);
-                    }
+                var containers = Game.spawns.Spawn1.room.find(FIND_STRUCTURES, { 
+                    filter: function(c) { 
+                        return c.structureType == STRUCTURE_CONTAINER && 
+                            c.store.energy > 0;
+                        }
+                    });
+                var container = creep.pos.findClosestByRange(containers);
+                if(creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container);    
                 }
             }
         }
-	}
+        else {
+            
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+        }
+    }
 };
 
 module.exports = roleHarvester;
