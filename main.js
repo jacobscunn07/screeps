7cf668d5b5f0b4f6f0e3659f0ff8752910479f03
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-21 20:10:37 -0500

    clean up code for harvester to find place to dump energy

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ module.exports.loop = function () {
         return this.pos.findClosestByPath(FIND_MY_SPAWNS);
     };
 
+    Creep.prototype.findClosestPlaceToDumpEnergy = function() {
+        return this.room.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+            }
+        });
+    };
+
     Creep.prototype.findClosestContainerThatIsNotFull = function() {
         return this.pos.findClosestByPath(FIND_STRUCTURES, {
             filter: function(c) {
--- role.harvester.js
+++ role.harvester.js
@@ -30,17 +30,9 @@ var roleHarvester = {
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
+            var target = creep.findClosestPlaceToDumpEnergy();
+            if(target && creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(target);
             }
         }
     }
