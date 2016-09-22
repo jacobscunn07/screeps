205436ce016b1c18a3786daed517ae6bbd1107b1
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-21 19:18:50 -0500

    add find sources creep extension

--- main.js
+++ main.js
@@ -7,25 +7,29 @@ var helpers = require('global.helpers');
 
 module.exports.loop = function () {
     Creep.prototype.sayHi = function(){this.say("hi")};
-    
+
     Creep.prototype.findClosestSpawn = function() {
         return this.pos.findClosestByPath(FIND_MY_SPAWNS);
     };
-    
+
     Creep.prototype.findClosestContainer = function() {
-        return this.pos.findClosestByPath(FIND_STRUCTURES, { 
-            filter: function(c) { 
+        return this.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: function(c) {
                 return c.structureType == STRUCTURE_CONTAINER && c.store.energy < c.storeCapacity;
             }
         });
     };
 
     Creep.prototype.findClosestContainerWithEnergy = function() {
-        return this.pos.findClosestByPath(FIND_STRUCTURES, { 
-            filter: function(c) { 
+        return this.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: function(c) {
                 return c.structureType == STRUCTURE_CONTAINER && c.store.energy < c.storeCapacity && c.store.energy > 0;
             }
         });
+    };
+
+    Creep.prototype.findSources = function() {
+          return this.pos.find(FIND_SOURCES);
     };
 
     Creep.prototype.findClosestSource = function() {
