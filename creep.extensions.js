a6f0d0bc3abd6b882d97041eb833f46999cb1f34
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-24 19:50:42 -0500

    refactor creep extensions into its own file

---
+++ creep.extensions.js
@@ -0,0 +1,67 @@
+var extensions = {
+    register: function() {
+        Creep.prototype.findClosestSpawn = function() {
+            return this.pos.findClosestByPath(FIND_MY_SPAWNS);
+        };
+
+        Creep.prototype.findClosestPlaceToDumpEnergy = function() {
+            return this.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+        };
+
+        Creep.prototype.getStorage = function() {
+            return this.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_STORAGE && structure.store.energy < structure.storeCapacity);
+                }
+            });
+        };
+
+        Creep.prototype.findClosestContainerThatIsNotFull = function() {
+            return this.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: function(c) {
+                    return c.structureType == STRUCTURE_CONTAINER && c.store.energy < c.storeCapacity;
+                }
+            });
+        };
+
+        Creep.prototype.findClosestContainerWithEnergy = function() {
+            return this.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: function(c) {
+                    return c.structureType == STRUCTURE_CONTAINER && c.store.energy > 0;
+                }
+            });
+        };
+
+        Creep.prototype.findSources = function() {
+            return this.room.find(FIND_SOURCES);
+        };
+
+        Creep.prototype.findClosestSource = function() {
+            return this.pos.findClosestByPath(FIND_SOURCES);
+        };
+
+        Creep.prototype.findClosestStructureNeedingRepair = function() {
+            return this.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: function(s) {
+                    return s.hits < (s.hitsMax * .7)
+                }
+            });
+        };
+
+        Creep.prototype.findClosestConstructionSite = function() {
+            return this.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        };
+
+        Creep.prototype.placeRoadUnderMe = function() {
+            return this.pos.createConstructionSite(STRUCTURE_ROAD);
+        };
+    }
+};
+
+module.exports = extensions;
--- main.js
+++ main.js
@@ -4,68 +4,10 @@ var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
 var roleMiner = require('role.miner');
 var helpers = require('global.helpers');
+var creepExtensions = require('creep.extensions');
 
-module.exports.loop = function () {
-    Creep.prototype.sayHi = function(){this.say("hi")};
-
-    Creep.prototype.findClosestSpawn = function() {
-        return this.pos.findClosestByPath(FIND_MY_SPAWNS);
-    };
-
-    Creep.prototype.findClosestPlaceToDumpEnergy = function() {
-        return this.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-            }
-        });
-    };
-
-    Creep.prototype.getStorage = function() {
-        return this.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_STORAGE && structure.store.energy < structure.storeCapacity);
-            }
-        });
-    };
-
-    Creep.prototype.findClosestContainerThatIsNotFull = function() {
-        return this.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: function(c) {
-                return c.structureType == STRUCTURE_CONTAINER && c.store.energy < c.storeCapacity;
-            }
-        });
-    };
-
-    Creep.prototype.findClosestContainerWithEnergy = function() {
-        return this.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: function(c) {
-                return c.structureType == STRUCTURE_CONTAINER  && c.store.energy > 0;
-            }
-        });
-    };
-
-    Creep.prototype.findSources = function() {
-          return this.room.find(FIND_SOURCES);
-    };
-
-    Creep.prototype.findClosestSource = function() {
-        return this.pos.findClosestByPath(FIND_SOURCES);
-    };
-
-    Creep.prototype.findClosestStructureNeedingRepair = function() {
-        return this.pos.findClosestByPath(FIND_STRUCTURES, {filter: function(s) {return s.hits < (s.hitsMax*.7)}});
-    };
-
-    Creep.prototype.findClosestConstructionSite = function() {
-        return this.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-    };
-
-    Creep.prototype.placeRoadUnderMe = function() {
-      return this.pos.createConstructionSite(STRUCTURE_ROAD);
-    };
-
+module.exports.loop = function() {
+    creepExtensions.register();
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
@@ -73,48 +15,54 @@ module.exports.loop = function () {
     var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
     var miners = _.filter(Game.creeps, (creep) => creep.memory.role == 'miner');
 
-    if(miners.length < 2) {
-        roleMiner.create();
-    }
-
-    if(harvesters.length < 2) {
-        roleHarvester.create();
-    }
-
-    if(upgraders.length < 6) {
+    if (upgraders.length < 6) {
         roleUpgrader.create();
     }
 
-    if(builders.length < 3 && Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES).length > 0) {
+    if (builders.length < 3 && Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES).length > 0) {
         roleBuilder.create();
-    }
-    else if (Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES).length == 0) {
-        _.forEach(builders, function(builder) { builder.suicide(); });
+    } else if (Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES).length == 0) {
+        _.forEach(builders, function(builder) {
+            builder.suicide();
+        });
     }
 
-    var needsRepairCount = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {filter: function(s){return s.hits < (s.hitsMax*.7)}}).length;
-    if(repairers.length < 2 && needsRepairCount > 0){
+    var needsRepairCount = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
+        filter: function(s) {
+            return s.hits < (s.hitsMax * .7)
+        }
+    }).length;
+    if (repairers.length < 2 && needsRepairCount > 0) {
         roleRepairer.create();
-    }
-    else if (needsRepairCount == 0) {
-        _.forEach(repairers, function(repairer){ repairer.suicide(); });
+    } else if (needsRepairCount == 0) {
+        _.forEach(repairers, function(repairer) {
+            repairer.suicide();
+        });
     }
 
-    for(var name in Game.creeps) {
+    if (harvesters.length < 2) {
+        roleHarvester.create();
+    }
+
+    if (miners.length < 2) {
+        roleMiner.create();
+    }
+
+    for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
+        if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'upgrader') {
+        if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
-        if(creep.memory.role == 'builder') {
+        if (creep.memory.role == 'builder') {
             roleBuilder.run(creep);
         }
-        if(creep.memory.role == 'repairer') {
+        if (creep.memory.role == 'repairer') {
             roleRepairer.run(creep);
         }
-        if(creep.memory.role == 'miner') {
+        if (creep.memory.role == 'miner') {
             roleMiner.run(creep);
         }
     }
