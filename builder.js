22405e87b70def03f3b224744e1dcb7dba0df908
Jacob Cunningham <jacob.cunningham@live.com>
2018-05-25 23:16:00 -0500

    Cleaned up builder code

--- builder.js
+++ builder.js
@@ -16,28 +16,46 @@ class Builder {
     }
 
     run() {
+        this.updateMemory();
+        if(this.isBuilding()) {
+            this.buildStructure();
+        }
+        else {
+            this.getEnergy();
+        }
+    }
+
+    updateMemory() {
         if (this.creep.memory.building && this.creep.carry.energy == 0) {
             this.creep.memory.building = false;
         }
         if (!this.creep.memory.building && this.creep.carry.energy == this.creep.carryCapacity) {
             this.creep.memory.building = true;
         }
+    }
 
-        if (this.creep.memory.building) {
-            var targets = this.creep.room.find(FIND_CONSTRUCTION_SITES);
-            if (targets.length) {
-                if (this.creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    this.creep.moveTo(targets[0]);
-                }
+    isBuilding() {
+        return this.creep.memory.building;
+    }
+
+    buildStructure() {
+        var structures = this.creep.room.find(FIND_CONSTRUCTION_SITES);
+        if (structures.length) {
+            var structure = _.first(structures);
+            if (this.creep.build(structure) == ERR_NOT_IN_RANGE) {
+                this.creep.moveTo(structure);
             }
-        } else {
-            var container = this.creep.findClosestContainerWithEnergyToMe();
+        }
+    }
+
+    getEnergy() {
+        var container = this.creep.findClosestContainerWithEnergyToMe();
+        if(container) {
             this.creep.tryGetEnergyFromContainer(container);
-
-            if(!container) {
-                var source = this.creep.findClosestSourceToMe();
-                this.creep.tryMineEnergySource(source);
-            }
+        }
+        else {
+            var source = this.creep.findClosestSourceToMe();
+            this.creep.tryMineEnergySource(source);
         }
     }
 }
